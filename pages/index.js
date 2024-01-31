675027f4645e70143934b5428ba4c9ea61a88cd1
dolinskih <047hubert@gmail.com>
2024-01-31 20:56:27 +0100

    Przygotowanie środowiska Next.JS

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import styles from '../styles/home.module.scss';
+
+export default function Home() {
+  return (
+    <Layout>
+       <Head>
+        <title>Scribe - Strona główna</title>
+      </Head>
+      <header>
+
+      </header>
+    </Layout>
+  );
+}
